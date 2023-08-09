e73e952c126035cb7cc4035f6daddc9bf3aca2b6
FelpsCorrea <felipecorrealopes1405@gmail.com>
2023-08-09 20:28:38 -0300

    Aula 3 feita

---
+++ introduction/2/main.go
@@ -0,0 +1,12 @@
+package main
+
+var (
+	b bool
+	c int
+	d string = "default"
+)
+
+func main() {
+	a := "abacate"
+	println(a)
+}
---
+++ introduction/3/main.go
@@ -0,0 +1,16 @@
+package main
+
+type ID int
+
+var (
+	b bool
+	c int
+	d string = "default"
+	e ID     = 1
+)
+
+func main() {
+	a := "abacate"
+	println(a)
+	println(e)
+}
