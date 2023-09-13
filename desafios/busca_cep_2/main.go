12616e3a3931391dd41dc934c85e8ffc095bb56b
FelpsCorrea <felipecorrealopes1405@gmail.com>
2023-09-12 21:29:22 -0300

    Aula http iniciada

--- desafios/busca_cep_2/main.go
+++ desafios/busca_cep_2/main.go
@@ -4,12 +4,28 @@ import "net/http"
 
 func main() {
 	// Definição de rota
-	http.HandleFunc("/", BuscaCEP)
+	http.HandleFunc("/", BuscaCepHandler)
 	// Abrindo servidor na porta 8080
 	http.ListenAndServe(":8080", nil)
 }
 
 // w é o retorno ao usuário e r é como o "Body"
-func BuscaCEP(w http.ResponseWriter, r *http.Request) {
+// func BuscaCEP(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello, World!"))
+// }
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, World!"))
 }
