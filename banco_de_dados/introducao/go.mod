cc9f8ea324c6008f7c7d1b72a25a6003d7492951
FelpsCorrea <felipecorrealopes1405@gmail.com>
2024-02-24 00:21:16 -0300

    Primeiro insert no banco com Go

--- banco_de_dados/introducao/go.mod
+++ banco_de_dados/introducao/go.mod
@@ -1,5 +1,8 @@
-module github.com/FelpsCorrea/GoExpertPostgraduation/banco_de_dados/introducao
+module github.com/FelpsCorrea/GoExpertPostgraduation/tree/main/banco_de_dados/introducao
 
 go 1.21.0
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.6.0
+)
--- banco_de_dados/introducao/main.go
+++ banco_de_dados/introducao/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
@@ -12,12 +15,43 @@ type Product struct {
 
 func NewProduct(name string, price float64) *Product {
 	return &Product{
-		ID:    uuid.New().String,
+		ID:    uuid.New().String(),
 		Name:  name,
 		Price: price,
 	}
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3307)/goexpert")
+	if err != nil {
+		panic(err)
+	}
 
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1899.90)
+	err = insertProduct(db, *product)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product Product) error {
+
+	// os "?" protegem o banco de SQL injection
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES (?, ?, ?)")
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// Aqui os "?" serão substituídos pelos dados da struct
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
